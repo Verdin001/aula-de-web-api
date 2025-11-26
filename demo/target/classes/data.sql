90fbc5d089b551e34ff3697c9758a8c8f43dc685
Verdin001 <lucatone0011@gmail.com>
2025-11-26 18:16:26 -0300

    Add files via upload

---
+++ demo/target/classes/data.sql
@@ -0,0 +1,7 @@
+create table tb_produto(
+                           id varchar(255) not null primary key,
+                           nome varchar (50) not null ,
+                           descricao varchar(300),
+                           preco decimal(18,2),
+                           quantidade int
+);
