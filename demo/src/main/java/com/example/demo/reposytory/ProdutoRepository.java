f6043b9232e456d34685febed5e508485eb1fa72
Verdin001 <lucatone0011@gmail.com>
2025-11-26 18:15:45 -0300

    Delete demo directory

--- demo/src/main/java/com/example/demo/reposytory/ProdutoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.reposytory;
-
-import com.example.demo.model.Produto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProdutoRepository extends JpaRepository<Produto, String> {
-}
